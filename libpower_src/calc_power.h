59facbf90e2e36231affc7e4b8eb50ba341431bc
Fredrik Strömbäck <fredde.stromback@gmail.com>
2020-10-23 23:46:38 +0200

    Removes \"R\" from include-guard

--- libpower_src/calc_power.h
+++ libpower_src/calc_power.h
@@ -1,5 +1,5 @@
-#ifndef __CALC_POWER_R__
-#define __CALC_POWER_R__
+#ifndef __CALC_POWER__
+#define __CALC_POWER__
 
 float calc_power_r(float volt, float resistance);
 float calc_power_i(float volt, float current);
