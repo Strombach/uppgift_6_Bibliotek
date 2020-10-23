4f0252174565d4be49940d1ecc03fb431414cc4c
Fredrik Strömbäck <fredde.stromback@gmail.com>
2020-10-23 23:09:19 +0200

    adds calc_power_i"

--- libpower_src/calc_power.h
+++ libpower_src/calc_power.h
@@ -2,5 +2,6 @@
 #define __CALC_POWER_R__
 
 float calc_power_r(float volt, float resistance);
+float calc_power_r(float volt, float current);
 
 #endif
---
+++ libpower_src/calc_power_i.c
@@ -0,0 +1,4 @@
+float calc_power_r(float volt, float current)
+{
+  return volt * current;
+}
